caae4d25ec889308c08e0288c98df2ce0d3f94bc
VInicius <viniciusvl>
2025-05-07 12:41:11 -0300

    update DB

--- Backend/banco.py
+++ Backend/banco.py
@@ -5,7 +5,7 @@ run_sql(
         CREATE TABLE IF NOT EXISTS users (
         
         username VARCHAR(24) PRIMARY KEY,
-        email_users VARCHAR(255) NOT NULL,
+        email_user VARCHAR(255) NOT NULL,
         password_user VARCHAR(255) NOT NULL
         
         )   
